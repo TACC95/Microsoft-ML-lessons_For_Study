51951ac7009e9f07e36dc6ff5874aba899a5f413
TACC95 <KL1452@outlook.com>
2026-09-19 23:42:08 +0800

    文件更新

---
+++ translations/zh-CN/3-Web-App/1-Web-App/web-app/app.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+loc = './translations/zh-CN/3-Web-App/1-Web-App/'
+
+model = pickle.load(open(loc + "ufo-model.pkl", "rb"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    countries = ["Australia", "Canada", "Germany", "UK", "US"]
+
+    return render_template(
+        "index.html", prediction_text="Likely country: {}".format(countries[output])
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
